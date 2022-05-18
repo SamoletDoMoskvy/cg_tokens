672c650c8001fad76810e7280b5201486debdbae
Alexander <aleksandr.tugay.02@gmail.com>
2022-05-18 23:09:10 +0300

    master: Added Token model in admin panel

--- tokens_app/admin.py
+++ tokens_app/admin.py
@@ -1,3 +1,31 @@
-from django.contrib import admin
+from django.contrib.admin import (
+    ModelAdmin,
+    register,
+)
 
-# Register your models here.
+from .models import Token
+
+
+@register(Token)
+class TokenModelAdmin(ModelAdmin):
+    fields = (
+        'id',
+        'address',
+        'name',
+        'symbol',
+        'price_in_dollars',
+        '_created_at',
+        '_updated_at',
+    )
+    list_display = (
+        'id',
+        'address',
+        'name',
+        'symbol',
+        'price_in_dollars',
+    )
+    readonly_fields = (
+        'id',
+        '_created_at',
+        '_updated_at'
+    )
